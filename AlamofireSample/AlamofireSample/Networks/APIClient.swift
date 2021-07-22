83dac134f2dbe095cc0cb1b269347509ca344ed0
김태현 <taehyk528@gmail.com>
2021-07-22 23:22:56 +0900

    [FEAT] Alamofire Custom Request
    - URLRequestConvertible 이용한 커스텀 Request 연습

---
+++ AlamofireSample/AlamofireSample/Networks/APIClient.swift
@@ -0,0 +1,33 @@
+//
+//  APIClient.swift
+//  AlamofireSample
+//
+//  Created by taehy.k on 2021/07/22.
+//
+
+import Foundation
+
+import Alamofire
+
+class APIClient {
+    
+    typealias onSuccess<T> = ((T) -> Void)
+    typealias onFailure = ((_ error: Error) -> Void)
+    
+    static func performRequest<T>(_ object: T.Type,
+                                  router: APIRouter,
+                                  success: @escaping onSuccess<T>,
+                                  failure: @escaping onFailure) where T: Decodable {
+        AF.request(router)
+            .validate(statusCode: 200..<500)
+            .responseDecodable(of: object) { response in
+                switch response.result {
+                case .success:
+                    success(response.value!)
+                case .failure(let err):
+                    failure(err)
+                }
+        }
+    }
+    
+}
